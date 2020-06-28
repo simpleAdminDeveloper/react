e6be8fc6080cdd89445935d3405ce1f546e65c49
Bek.Vikas <vikas.bek@paytm.com>
2020-06-28 23:02:59 +0530

    Updates

---
+++ precache-manifest.4b195b3db0524c8c2519742fd8933bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a29ddaaf82ba6bb9a043689fb080436",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5e83ed0a5a4bb4a24a6",
+    "url": "/static/css/main.415edbe9.chunk.css"
+  },
+  {
+    "revision": "7b555debf535f32f782e",
+    "url": "/static/js/2.9fc7c5d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9fc7c5d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5e83ed0a5a4bb4a24a6",
+    "url": "/static/js/main.e43ad147.chunk.js"
+  },
+  {
+    "revision": "a55fe9c0b312e2774e66",
+    "url": "/static/js/runtime-main.80c5e1e7.js"
+  }
+]);
