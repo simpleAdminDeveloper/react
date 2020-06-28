fffb4d983809efde452187ccfa07575eeb3b58a7
Bek.Vikas <vikas.bek@paytm.com>
2020-06-28 23:24:32 +0530

    card h2 change

--- src/component/card/card.component.jsx
+++ src/component/card/card.component.jsx
@@ -5,7 +5,7 @@ export const Card = (props) => {
     return (
         <div className='card-container'>
             <img src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`} alt="monster"/>
-            <h1 key={props.monster.id}>{props.monster.name}</h1>
+            <h2 key={props.monster.id}>{props.monster.name}</h2>
             <p>{props.monster.email}</p>
         </div>
     
